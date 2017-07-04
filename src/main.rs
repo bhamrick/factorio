ecf258d42e37c100a02f09d0eacc0c7436e4ae9d
Brian Hamrick <brian.c.hamrick@gmail.com>
2017-07-04 07:16:24 -0500

    Read part of the design

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,239 @@
 #[macro_use]
+extern crate lazy_static;
 extern crate nom;
 
 use std::fs::File;
+use std::collections::{HashMap, HashSet};
+use std::collections::hash_map::Entry;
 use std::str;
+use std::str::FromStr;
 use std::io::prelude::*;
 use std::vec::Vec;
 
 use nom::*;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Resource {
+    Coal,
+    Water,
+    SolidFuel,
+    HeavyOil,
+    LightOil,
+    Petroleum,
+    Steam,
+}
+
+lazy_static! {
+    static ref RESOURCE_NAMES : Vec<(Resource, &'static str)> = vec![
+        (Resource::Coal, "Coal"),
+        (Resource::Water, "Water"),
+        (Resource::SolidFuel, "Solid fuel"),
+        (Resource::HeavyOil, "Heavy oil"),
+        (Resource::LightOil, "Light oil"),
+        (Resource::Petroleum, "Petroleum"),
+        (Resource::Steam, "Steam"),
+    ];
+
+    static ref PROTO_RECIPES : Vec<ProtoRecipe<'static>> = vec![
+        // Recipes required for the coal liquefaction line
+        ProtoRecipe {
+            name: "Coal liquefaction",
+            inputs: vec![
+                (Resource::Coal, 10.0),
+                (Resource::HeavyOil, 25.0),
+                (Resource::Steam, 50.0),
+            ],
+            outputs: vec![
+                (Resource::HeavyOil, 35.0),
+                (Resource::LightOil, 15.0),
+                (Resource::Petroleum, 20.0),
+            ],
+            time: 5.0,
+        },
+        ProtoRecipe {
+            name: "Heavy oil cracking",
+            inputs: vec![
+                (Resource::HeavyOil, 40.0),
+                (Resource::Water, 30.0),
+            ],
+            outputs: vec![
+                (Resource::LightOil, 30.0),
+            ],
+            time: 3.0,
+        },
+        ProtoRecipe {
+            name: "Solid fuel (Light oil)",
+            inputs: vec![
+                (Resource::LightOil, 10.0),
+            ],
+            outputs: vec![
+                (Resource::SolidFuel, 1.0),
+            ],
+            time: 3.0,
+        },
+        ProtoRecipe {
+            name: "Solid fuel (Petroleum)",
+            inputs: vec![
+                (Resource::Petroleum, 20.0),
+            ],
+            outputs: vec![
+                (Resource::SolidFuel, 1.0),
+            ],
+            time: 3.0,
+        },
+        ProtoRecipe {
+            name: "Burning solid fuel",
+            inputs: vec![
+                (Resource::Water, 60.0),
+                (Resource::SolidFuel, 0.144),
+            ],
+            outputs: vec![
+                (Resource::Steam, 60.0),
+            ],
+            time: 1.0,
+        },
+    ];
+}
+
+impl Resource {
+    fn from_str(name: &str) -> Result<Resource, InputError> {
+        for &(resource, resource_name) in RESOURCE_NAMES.iter() {
+            // TODO: Make this comparison case insensitive
+            if name == resource_name {
+                return Ok(resource);
+            }
+        }
+        Err(InputError::new("Unknown resource"))
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ResourceLine<'a> {
+    name: &'a str,
+    resource_type: Resource,
+    index: usize,
+}
+
+#[derive(Debug, Clone)]
+struct Building<'a> {
+    name: &'a str,
+    recipe: Recipe<'a>,
+    energy_consumption: f32,
+    index: usize,
+}
+
+#[derive(Debug, Clone)]
+struct Recipe<'a> {
+    name: &'a str,
+    inputs: Vec<(ResourceLine<'a>, f32)>,
+    outputs: Vec<(ResourceLine<'a>, f32)>,
+    time: f32,
+}
+
+#[derive(Debug, Clone)]
+struct ProtoRecipe<'a> {
+    name: &'a str,
+    inputs: Vec<(Resource, f32)>,
+    outputs: Vec<(Resource, f32)>,
+    time: f32,
+}
+
+#[derive(Debug, Clone)]
+struct InputError {
+    message: String,
+}
+
+impl From<std::str::Utf8Error> for InputError {
+    fn from(_ : std::str::Utf8Error) -> InputError {
+        InputError::new("Unicode error")
+    }
+}
+
+impl InputError {
+    fn new(msg: &str) -> InputError {
+        InputError {
+            message: String::from_str(msg).unwrap(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Design<'a> {
+    resource_lines: HashMap<&'a str, ResourceLine<'a>>,
+    buildings: Vec<Building<'a>>,
+    input_lines: HashSet<&'a str>,
+    output_lines: HashSet<&'a str>,
+    next_index: usize,
+}
+
+impl<'a> Design<'a> {
+    fn new() -> Design<'a> {
+        Design {
+            resource_lines: HashMap::new(),
+            buildings: Vec::new(),
+            input_lines: HashSet::new(),
+            output_lines: HashSet::new(),
+            next_index: 0,
+        }
+    }
+
+    fn get_line(&mut self, resource_type: Resource, name: &'a str) -> Result<ResourceLine<'a>, InputError> {
+        match self.resource_lines.entry(name) {
+            Entry::Occupied(ent) => {
+                let existing_line = ent.get();
+                if existing_line.resource_type == resource_type {
+                    Ok(existing_line.clone())
+                } else {
+                    Err(InputError::new("Resource type mismatch"))
+                }
+            },
+            Entry::Vacant(ent) => {
+                let new_line = ResourceLine {
+                    name: name,
+                    resource_type: resource_type,
+                    index: self.next_index,
+                };
+                self.next_index += 1;
+                ent.insert(new_line.clone());
+                Ok(new_line)
+            },
+        }
+    }
+
+    fn from_data(data: Vec<Data<'a>>) -> Result<Design<'a>, InputError> {
+        let mut design = Design::new();
+        for datum in data {
+            if datum.value == b"Inputs" {
+                for input_datum in datum.children {
+                    let resource_type_str = std::str::from_utf8(input_datum.value)?;
+                    let resource_type = Resource::from_str(resource_type_str)?;
+                    for input_line in input_datum.children {
+                        if !input_line.children.is_empty() {
+                            return Err(InputError::new("Unexpected child of input line name"));
+                        }
+                        let input_line_name = std::str::from_utf8(input_line.value)?;
+                        let resource_line = design.get_line(resource_type, input_line_name)?;
+                        design.input_lines.insert(resource_line.name);
+                    }
+                }
+            } else if datum.value == b"Outputs" {
+                for output_datum in datum.children {
+                    let resource_type_str = std::str::from_utf8(output_datum.value)?;
+                    let resource_type = Resource::from_str(resource_type_str)?;
+                    for output_line in output_datum.children {
+                        if !output_line.children.is_empty() {
+                            return Err(InputError::new("Unexpected child of output line name"));
+                        }
+                        let output_line_name = std::str::from_utf8(output_line.value)?;
+                        let resource_line = design.get_line(resource_type, output_line_name)?;
+                        design.output_lines.insert(resource_line.name);
+                    }
+                }
+            }
+        }
+        Ok(design)
+    }
+}
 
 #[derive(Debug, Clone)]
 struct Data<'a> {
@@ -130,6 +357,7 @@ fn main() {
 
     let clean_contents = clean(contents);
 
-    let result = Data::from_bytes(&clean_contents);
-    println!("{:?}", result);
+    let parsed_data = Data::from_bytes(&clean_contents);
+    let design = Design::from_data(parsed_data.unwrap());
+    println!("{:?}", design);
 }
